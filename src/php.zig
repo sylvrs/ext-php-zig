a135405c2b7e4da059f21b71b8c5204a3f5a4a04
Em Jordan <sylvrs@proton.me>
2023-08-11 16:42:47 -0500

    chore: update documentation about exposed imports

--- src/php.zig
+++ src/php.zig
@@ -1,5 +1,8 @@
-// Expose these namespaces to the users of this library.
+/// `internal` exposes PHP's internal functions and structs.
 pub const internal = @import("internal.zig");
+/// `zend` contains the definitions of the Zend's structs and functions.
 pub const zend = @import("zend.zig");
+/// `utils` contains various utility functions that are used throughout the library but could be useful to the users as well.
 pub const utils = @import("utils.zig");
+/// `Module` represents the library's wrapper around the Zend's ModuleEntry struct.
 pub const Module = @import("Module.zig");
